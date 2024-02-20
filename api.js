696e5763dc793a81d2b7564f4d8b4edd977b2750
abdelkebir7y <abdouhassou92@gmail.com>
2024-02-20 21:45:52 +0100

    add index.html

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const api = express();
+api.use(express.static(__dirname + "/public"));
+api.use(cors({}));
 
 api.get("/login", (req, res) => {
   res.send("Hello world");
--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const apiServer = require("./api.js");
 const sockets = require("./sockets.js");
 
 const httpServer = http.createServer(apiServer);
-const socketServer = new Server(httpServer, { path: "/auth/socket.io" });
+const socketServer = new Server(httpServer, {
+  path: "/auth/socket.io",
+  cors: {
+    origin: "*",
+  },
+});
 
 const PORT = 3000;
 httpServer.listen(PORT);
